f5bd4fec80a2a19fc48ccf191001a4533a4c185f
Prathmesm13 <prathmeshmahakal35@gmail.com>
2024-09-17 19:44:18 +0530

    logout corrected

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-      origin: "http://localhost:3000", // Your frontend URL
+      origin: "https://mern-chat-app-g3nc.onrender.com", // Your frontend URL
       credentials: true, // Allow credentials (cookies) to be sent
     })
 );
